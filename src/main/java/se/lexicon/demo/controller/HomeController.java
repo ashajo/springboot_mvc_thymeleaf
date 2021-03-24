99dbb909575b9ce615df4bfb6f8a02684bf299a9
asha joseph <ashajo183@gmail.com>
2021-03-24 09:42:53 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/demo/controller/HomeController.java
@@ -0,0 +1,17 @@
+package se.lexicon.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+public String gotoIndexPage(){
+        return "index";
+
+}
+
+}
+
+
+
